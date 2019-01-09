7066c4c23f9236a273b4f0b000da7b2a06c2ef6b
권병윤 <sksggg123@gmail.com>
2019-01-09 20:37:22 +0900

    project init

---
+++ src/main/java/com/goquality/storage/DataStorageApplication.java
@@ -0,0 +1,14 @@
+package com.goquality.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStorageApplication.class, args);
+	}
+
+}
+
